14f373ff91e01c6837a459446b9ef77927a3dcee
Hasan Su'udi <koetik@gmail.com>
2022-05-20 18:02:21 +0700

    bug fixing add cart

--- app/controllers/cart.controller.js
+++ app/controllers/cart.controller.js
@@ -2,6 +2,7 @@ const db = require("../models");
 const controller = require("./controller");
 
 const Cart = db.cart;
+const Product = db.product;
 const Op = db.Sequelize.Op;
 
 exports.getCart = (req, res) => {
@@ -26,33 +27,86 @@ exports.getCart = (req, res) => {
 };
 
 exports.create = async (req, res) => {
-    
+
     var getCart = await Cart.findOne({
         where: {
             product_id: req.body.product_id
         },
     });
-    var qty = (getCart)?getCart.qty + 1:1
+    var getProduct = await Product.findByPk(req.body.product_id);
+
+    if(getProduct.stock == 0) {
+        var data = controller.apiResponse(("Stok Habis"), false)
+        return res.status(500).send(data);
+    }
+
+
+    var qty = (getCart) ? getCart.qty + 1 : 1
 
     const cartData = {
-        product_id : req.body.product_id,
+        product_id: req.body.product_id,
         qty: qty,
     };
 
-    Cart.create(cartData)
-        .then(data => {
-            Cart.findByPk(data.id).then(re => {
-                data = cartSingleTransform(re);
-                data = controller.apiResponse(data, true)
-                res.send(data);
+    
+    
+    if (getCart) {
+        Cart.update(cartData, {
+                where: {
+                    id: getCart.id
+                }
             })
-        })
-        .catch(err => {
-            var data = controller.apiResponse((err.message || "Some error occurred while creating the Product."), false)
-            return res.status(500).send(data);
-        });
+            .then(data => {
+                Cart.findByPk(getCart.id).then(re => {
+                    getProduct.update({
+                        stock: (getProduct.stock - 1)
+                    })
+                    data = cartSingleTransform(re);
+                    data = controller.apiResponse(data, true)
+                    res.send(data);
+                })
+            })
+            .catch(err => {
+                var data = controller.apiResponse((err.message || "Some error occurred while creating the Product."), false)
+                return res.status(500).send(data);
+            });
 
+
+    } else {
+        Cart.create(cartData)
+            .then(data => {
+                Cart.findByPk(data.id).then(re => {
+                    getProduct.update({
+                        stock: (getProduct.stock - 1)
+                    })
+                    data = cartSingleTransform(re);
+                    data = controller.apiResponse(data, true)
+                    res.send(data);
+                })
+
+                
+            })
+            .catch(err => {
+                var data = controller.apiResponse((err.message || "Some error occurred while creating the Product."), false)
+                return res.status(500).send(data);
+            });
+    }
+    
 }
+
+exports.clear = async (req, res) => {
+
+    var getCart = await Cart.findOne({
+        where: {
+            product_id: req.body.product_id
+        },
+    });
+    var getProduct = await Product.findByPk(req.body.product_id);
+    
+    
+    
+}
+
 
 
 
@@ -68,7 +122,6 @@ function cartMultipleTransform(dt) {
 }
 
 function cartSingleTransform(dt) {
-
     return {
         id: dt.id,
         qty: dt.qty,
